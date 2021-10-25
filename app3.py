d146793144d6af8c66c27a86c4599a5f53470cca
Riya <83034710+riyaflix@users.noreply.github.com>
2021-10-25 16:02:07 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from app3'
+    return 'Welcome to Server 3'
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5003)

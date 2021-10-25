9939b15dbd03a52447f2dfdc1fcb717ee0586212
Riya <83034710+riyaflix@users.noreply.github.com>
2021-10-25 15:58:25 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from app1'
+    return 'Welcome to Server 1'
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5001)
